f4abe09b1ff0b4b6c2a69c2170e92c278398f041
Carlos Enrique Muniz Cuza <38991005+cmcuza@users.noreply.github.com>
2025-07-04 10:35:19 +0200

    Update shared_structs.zig

--- src/utilities/shared_structs.zig
+++ src/utilities/shared_structs.zig
@@ -13,8 +13,9 @@
 // limitations under the License.
 
 //! Contains all shared variables and structures used across TerseTS.
+
 const std = @import("std");
-pub const HashMap = std.HashMap;
+const HashMap = std.HashMap;
 
 /// Margin to adjust the error bound for numerical stability. Reducing the error bound by this
 /// margin ensures that all the elements of the decompressed time series are within the error bound
@@ -34,10 +35,10 @@ pub const Segment = struct {
     end_point: DiscretePoint,
 };
 
-/// Linear function of the form y = slope*x+intercept.
+/// Linear function of the form y = slope*x+intercept. It uses f80 for numerical stability.
 pub const LinearFunction = struct {
-    slope: f64,
-    intercept: f64,
+    slope: f80,
+    intercept: f80,
 };
 
 /// `Point` is a point represented by `time` and `value`. `time` is of datatype `time_type`.
@@ -46,8 +47,8 @@ fn Point(comptime time_type: type) type {
 }
 
 /// `SegmentMetadata` stores the information about an approximated segment during the execution
-/// of Sim-Piece. It stores the starting time of the segment in `start_time`, the
-/// `intercept` point used to create the linear function approximation, and the slopes of
+/// of Sim-Piece and Mix-Piece. It stores the starting time of the segment in `start_time`, the
+/// `interception` point used to create the linear function approximation, and the slopes of
 /// the upper and lower bounds that constraint the linear approximation in that segment.
 pub const SegmentMetadata = struct {
     start_time: usize,
@@ -55,3 +56,22 @@ pub const SegmentMetadata = struct {
     upper_bound_slope: f64,
     lower_bound_slope: f64,
 };
+
+/// `HashF64Context` provides context for hashing and comparing `f64` values for use in `HashMap`.
+/// This context is essential when using `f64` as keys in a `HashMap`. It defines how the keys are
+/// hashed and compared for equality.
+pub const HashF64Context = struct {
+    /// Hashes a `f64` value by bitcasting it to `u64`.
+    pub fn hash(_: HashF64Context, value: f64) u64 {
+        return @as(u64, @bitCast(value));
+    }
+    /// Compares two `f64` values for equality.
+    pub fn eql(_: HashF64Context, value_one: f64, value_two: f64) bool {
+        return value_one == value_two;
+    }
+};
+
+/// Returns a HashMap with key type f64 and `value_type` given by the user.
+pub fn HashMapf64(comptime value_type: type) type {
+    return HashMap(f64, value_type, HashF64Context, std.hash_map.default_max_load_percentage);
+}
