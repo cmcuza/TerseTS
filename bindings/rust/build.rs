baecac6d6f0fbad0941615ffe180325fbf7f3be3
Søren Kejser Jensen <devel@kejserjensen.dk>
2026-02-03 07:37:41 +0100

    Make use same optimization level and link to static

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -13,29 +13,51 @@
 // limitations under the License.
 
 use std::env;
+use std::fs;
 use std::process::{self, Command};
 
-/// Compile TerseTS into a statically linked library and link it.
+/// Compile TerseTS into a statically linked library and link it. unwrap() is deliberately used, as
+/// recovery is not possible if any of the unwrapped operations fail, so better to fail immediately.
 fn main() {
-    // Safe to unwrap as Rust bindings are stored two folders deep.
-    let current_diroty = env::current_dir().unwrap();
-    let repository_root = current_diroty.parent().unwrap().parent().unwrap();
+    // Compute the repositories root for running zig build and linking.
+    let current_directory = env::current_dir().unwrap();
+    let repository_root = current_directory.parent().unwrap().parent().unwrap();
 
+    // Make the optimization level of TerseTS and Rust bindings match.
+    let build_profile = env::var("PROFILE").unwrap();
+    let optimize = match build_profile.as_str() {
+        "debug" => "-Doptimize=Debug",
+        "release" => "-Doptimize=ReleaseFast",
+        build_profile => {
+            println!(
+                "cargo::error=Profile must be debug (dev) or release, not {}.",
+                build_profile
+            );
+            process::exit(1);
+        }
+    };
+
+    // Build the TerseTS library into a statically linked library.
     let output = Command::new("zig")
         .current_dir(repository_root)
-        .args(["build", "-Dlinking=static"])
+        .args(["build", "-Dlinking=static", optimize])
         .output()
         .unwrap();
 
     if !output.status.success() {
         // Output is captured by cargo and used as commands.
         println!(
-            "cargo::error={} {}.",
-            "Failed to build TerseTS as a static library due to",
+            "cargo::error=Failed to build TerseTS as a static library due to {}.",
             String::from_utf8(output.stderr).unwrap()
         );
         process::exit(1);
     }
 
-    // TODO: Link the statically linked library.
+    // Specify the name and location of the TerseTS library for the linker.
+    let mut library_path = repository_root.to_path_buf();
+    library_path.push("zig-out");
+    library_path.push("lib");
+
+    println!("cargo::rustc-link-lib=static={}", "tersets");
+    println!("cargo:rustc-link-search=native={}", library_path.display());
 }
