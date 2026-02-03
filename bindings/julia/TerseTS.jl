753b17ed0d717d176b9018b501a502c906fceb8b
Søren Kejser Jensen <devel@kejserjensen.dk>
2026-02-03 20:13:09 +0100

    Finish draft implementation of Julia bindings

--- bindings/julia/TerseTS.jl
+++ bindings/julia/TerseTS.jl
@@ -16,6 +16,9 @@
 
 module TerseTS
 
+"""
+Mirror TerseTS `Method` enum.
+"""
 @enum Method begin
     PoorMansCompressionMidrange   = 0
     PoorMansCompressionMean       = 1
@@ -36,17 +39,26 @@ module TerseTS
     SerfQT                        = 16
 end
 
+"""
+A pointer to uncompressed values and the number of values.
+"""
 struct UncompressedValues
-    data::Ptr{Float64}
+    data::Ref{Float64}
     len::Csize_t
 end
 
+"""
+A pointer to compressed values and the number of bytes.
+"""
 struct CompressedValues
-    data::Ptr{UInt8}
+    data::Ref{UInt8}
     len::Csize_t
 end
 
-function find_library()
+"""
+Locate the TerseTS library for this operation.
+"""
+function findlibrary()
     if  Sys.isapple()
         println("Hello Mac")
     elseif Sys.isunix()
@@ -58,23 +70,49 @@ function find_library()
         error("Could not find TerseTS: looked '*{library_name}' in {library_folder}")
     end
 
-    "/home/kejser/Projects/2023-TerseTS/TerseTS/zig-out/lib/"
+    "/home/kejser/Projects/2023-TerseTS/TerseTS/zig-out/lib/libtersets.so"
 end
 
-function compress()
+"""
+Compress a `Vector` of `Float64` values with a selected TerseTS `method` according to `configuration`.
+"""
+function compress(uncompressed_values::AbstractVector{Float64}, method::Method, configuration::AbstractString) AbstractVector{UInt8}
+    uncompressed_values_ref = convert(Ref{Float64}, uncompressed_values)
+    uncompressed_values_struct = UncompressedValues(uncompressed_values_ref, length(uncompressed_values))
+    compressed_values_struct = CompressedValues(C_NULL, 0)
+
+    tersets_error = @ccall findlibrary().compress(uncompressed_values_struct::Ref{UncompressedValues}, compressed_values_struct::Ref{CompressedValues}, method::Method, configuration::Cstring)::Cint
+    if tersets_error != 0
+        throw("compress failed: $tersets_error")
+    end
+
+    compressed_values = convert(AbstractVector{UInt8}, uncompressed_values_struct.data)
+    @ccall findlibrary().freeCompressedValues(compressed_values_struct::Ref{CompressedValues})::Cvoid
+
+    compressed_values
 end
 
-function decompress()
+"""
+Decompress a TerseTS-compressed `Vector` into a `Vector` of `Float64`.
+"""
+function decompress(compressed_values::AbstractVector{UInt8}) AbstractVector{Float64}
+    compressed_values_ref = convert(Ref{UInt8}, compressed_values)
+    compressed_values_struct = CompressedValues(compressed_values_ref, length(compressed_values))
+    uncompressed_values_struct = UncompressedValues(C_NULL, 0)
+
+    tersets_error = @ccall findlibrary().decompress(compressed_values_struct::Ref{CompressedValues}, uncompressed_values_struct::Ref{UncompressedValues})::Cint
+    if tersets_error != 0
+        throw("decompress failed: $tersets_error")
+    end
+
+    uncompressed_values = convert(AbstractVector{Float64}, compressed_values_struct.data)
+    @ccall findlibrary().freeUncompressedValues(compressed_values_struct::Ref{UncompressedValues})::Cvoid
+
+    uncompressed_values
 end
 
-function freeCompressedValues()
-end
-
-function freeUncompressedValues()
-end
-
-function __init__()
-    find_library()
-end
+uncompressed_values = [1.0, 2.0, 3.0, 4.0, 5.0]
+compressed_values = compress(uncompressed_values, PoorMansCompressionMidrange, "{\" absolute_error_bound: 1.0 \"}")
+println(compressed_values)
 
 end
