9f3274bee3b882fc0257f6da83cf301ec9530856
Søren Kejser Jensen <devel@kejserjensen.dk>
2026-08-18 08:15:51 +0200

    Integrate unit and integration tests

--- build.zig
+++ build.zig
@@ -14,8 +14,10 @@
 
 const std = @import("std");
 const Build = std.Build;
-const Lang = std.Lang;
 const LinkMode = std.builtin.LinkMode;
+const Module = std.Build.Module;
+const OptimizeMode = std.builtin.OptimizeMode;
+const ResolvedTarget = Build.ResolvedTarget;
 
 pub fn build(b: *Build) void {
     const target = b.standardTargetOptions(.{});
@@ -36,7 +38,7 @@ pub fn build(b: *Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     // Create root module.
-    const root_module = create_module_using_tersets("src/capi.zig", target, optimize, pic);
+    const root_module = create_module_using_tersets(b, "src/capi.zig", target, optimize, pic);
 
     // Task for compilation.
     const library = b.addLibrary(.{
@@ -52,16 +54,10 @@ pub fn build(b: *Build) void {
 
     b.installArtifact(library);
 
-    // Task for running unit tests.
-    const unit_tests = b.addTest(.{
-        .root_module = root_module,
-    });
+    // Task for building and running tests.
+    const unit_tests = b.addTest(.{ .root_module = root_module });
     const run_unit_tests = b.addRunArtifact(unit_tests);
 
-    const unit_test_step = b.step("test", "Run unit tests");
-    unit_test_step.dependOn(&run_unit_tests.step);
-
-    // Task for creating and running integration tests.
     const integration_test_builder = b.addExecutable(.{
         .name = "integration_test_builder",
         .root_module = b.createModule(.{
@@ -72,12 +68,12 @@ pub fn build(b: *Build) void {
     });
     const build_integration_tests = b.addRunArtifact(integration_test_builder);
 
-    const integration_test_module = create_module_using_tersets("src/integration_tests.zig", target, optimize, null);
-
+    const integration_test_module = create_module_using_tersets(b, "src/integration_tests.zig", target, optimize, null);
     const integration_tests = b.addTest(.{ .root_module = integration_test_module });
     const run_integration_tests = b.addRunArtifact(integration_tests);
 
-    const test_step = b.step("integration_test", "Run integration tests");
+    const test_step = b.step("test", "Run unit and integration tests");
+    test_step.dependOn(&run_unit_tests.step);
     test_step.dependOn(&build_integration_tests.step);
     test_step.dependOn(&run_integration_tests.step);
 }
@@ -85,11 +81,11 @@ pub fn build(b: *Build) void {
 /// Create a module with the provided arguments and the additional configuration required to compile TerseTS.
 fn create_module_using_tersets(
     b: *Build,
-    root_source_file: []u8,
-    target: Build.ResolvedTarget,
-    optimize: Lang.OptimizeMode,
-    pic: Build.Pic,
-) Build.Module {
+    root_source_file: []const u8,
+    target: ResolvedTarget,
+    optimize: OptimizeMode,
+    pic: ?bool,
+) *Module {
     // Paths to external libraries. Include the PocketFFT source file directly in the build,
     // as it's a single C file with no dependencies.
     const pocketfft_path = b.path("lib/pocketfft");
@@ -105,4 +101,5 @@ fn create_module_using_tersets(
     integration_test_module.addIncludePath(pocketfft_path);
     integration_test_module.addCSourceFile(.{ .file = pocketfft_c_path });
     integration_test_module.link_libc = true;
+    return integration_test_module;
 }
